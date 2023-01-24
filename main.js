d7384de412929b89816048cb06b7c6f225b3f2b7
uthmancoder <akandeadebayo025@gmail.com>
2023-01-24 22:39:11 +0100

    added some little styling

--- main.js
+++ main.js
@@ -5,4 +5,9 @@ let secEl = document.querySelector(".seconds")
 let date = new Date;
 console.log(date);
 let secDeg = date.getHours()/60 * 360;
+let minDeg = date.getMinutes()/60 * 360;
+let hoursDeg = date.getMinutes()/12 * 360;
+
 secEl.style.transform = `rotate(10deg)`
+minEl.style.transform = `rotate(10deg)`
+hoursEl.style.transform = `rotate(10deg)`
