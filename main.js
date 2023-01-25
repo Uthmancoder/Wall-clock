aa7c0efbd15de3204412357aa191b8c76c55fd48
uthmancoder <akandeadebayo025@gmail.com>
2023-01-25 09:07:11 +0100

    second commit

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-let hoursEl = document.querySelector(".hours")
-let minEl = document.querySelector(".minutes")
-let secEl = document.querySelector(".seconds")
-
-let date = new Date;
-console.log(date);
-let secDeg = date.getHours()/60 * 360;
-let minDeg = date.getMinutes()/60 * 360;
-let hoursDeg = date.getMinutes()/12 * 360;
-
-secEl.style.transform = `rotate(10deg)`
-minEl.style.transform = `rotate(10deg)`
-hoursEl.style.transform = `rotate(10deg)`
+        let hoursEl = document.querySelector(".hours")
+        let minEl = document.querySelector(".minutes")
+        let secEl = document.querySelector(".seconds")
+        setInterval(() => {8
+            const date = new Date;
+            // console.log(date);
+            let secDeg = date.getSeconds()/ 60 * 360 - 90;
+           let minDeg = date.getMinutes()/ 60 * 360 - 90;
+           let hourDeg = date.getHours()/12 * 360 - 90;
+           secEl.style.transform = `rotate(${secDeg}deg)`
+           minEl.style.transform = `rotate(${minDeg}deg)`
+           hoursEl.style.transform = `rotate(${hourDeg}deg)`
+        }, 1000);
